d055c24b9d4d193201f46d2b5900356ca61bb120
speckitor <pasaloginov381@gmail.com>
2025-11-05 22:54:12 +0700

    token & lexer typedef + file_to_str function

--- cfg.h
+++ cfg.h
@@ -26,26 +26,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+#include <string.h>
 
 #define INITIAL_VARIABLES_NUM 32
 
-#define PARSING_STACK_SIZE 256
-
-typedef enum {
-    CFG_TYPE_INT = 0,
-    CFG_TYPE_DOUBLE,
-    CFG_TYPE_BOOL,
-    CFG_TYPE_STRING,
-    CFG_TYPE_STRUCT,
-    CFG_TYPE_LIST,
-    CFG_TYPE_COUNT,
-} Cfg_Value_Type;
-
+#define MAX_NAME_LENGTH 256
+#define MAX_VALUE_LENGTH 256
 typedef enum {
     CFG_ERROR_NONE = 0,
     CFG_ERROR_OPEN_FILE,
-    CFG_ERROR_LOAD_FILE,
+    CFG_ERROR_SYNTAX,
+    CFG_ERROR_VARIABLE_REDEFINITION,
+    CFG_ERROR_STACK_OVERFLOW,
     CFG_ERROR_PARSE_VALUE,
     CFG_ERROR_COUNT,
 } Cfg_Error_Type;
@@ -60,13 +52,11 @@ typedef struct {
 typedef struct Cfg_Variable Cfg_Variable;
 
 struct Cfg_Variable {
-    const char *name;
-    const char *value;
-
-    Cfg_Value_Type type;
-
-    Cfg_Variable *vars; // Nested elements
+    char *name;
+    char *value;
+    Cfg_Variable *vars;
     size_t vars_len;
+    size_t vars_cap;
 };
 
 typedef struct {
@@ -94,9 +84,67 @@ char *cfg_get_error_message(Cfg_Config *cfg);
 
 // #ifdef CFG_IMPLEMENTATION
 
-// Private functions
+// Private functions and types
 
-static int _cfg_open_file(Cfg_Config *cfg, const char *file_path)
+typedef enum {
+    CFG_TOKEN_NONE,
+    CFG_TOKEN_EQ,
+    CFG_TOKEN_SEMICOLON,
+    CFG_TOKEN_IDENTIFIER,
+    CFG_TOKEN_INT,
+    CFG_TOKEN_EOF,
+} Cfg_Token_Type;
+
+typedef struct {
+    Cfg_Token_Type type;
+    char *token;
+    size_t line;
+    size_t col;
+} Cfg_Token;
+
+typedef struct {
+    Cfg_Token *tokens;
+    Cfg_Token *current_token;
+    size_t token_len;
+    size_t token_cap;
+} Cfg_Lexer;
+
+static char *cfg__get_file_str(Cfg_Config *cfg)
+{
+    fseek(cfg->file, 0, SEEK_END);
+    long size = ftell(cfg->file);
+    fseek(cfg->file, 0, SEEK_SET);
+    char *file_str = malloc(sizeof(char) * (size + 1));
+    fread(file_str, sizeof(char), size, cfg->file);
+    file_str[size] = '\0';
+    return file_str;
+}
+
+static Cfg_Lexer *cfg__tokenize_file(Cfg_Config *cfg)
+{
+    // TODO
+}
+
+
+static int cfg__parse_context(Cfg_Config *cfg, Cfg_Variable *ctx)
+{
+    // TODO    
+}
+
+static void cfg__add_variable(Cfg_Variable *ctx, char *name, char *value)
+{
+    if (ctx->vars_len == ctx->vars_cap) {
+        ctx->vars_cap *= 2;
+        ctx->vars = realloc(ctx->vars, ctx->vars_cap);
+        if (!ctx->vars) {
+            perror("realloc");
+        }
+    }
+
+    ctx->vars[ctx->vars_len++].name = strdup(name);
+    ctx->vars[ctx->vars_len++].value = strdup(value);
+}
+static int cfg__open_file(Cfg_Config *cfg, const char *file_path)
 {
     FILE *file = fopen(file_path, "r");
     if (!file) {
@@ -107,16 +155,9 @@ static int _cfg_open_file(Cfg_Config *cfg, const char *file_path)
     return 0;
 }
 
-static int _cfg_parse_file(Cfg_Config *cfg)
+static int cfg__parse_file(Cfg_Config *cfg, Cfg_Variable *ctx)
 {
-    char val_stack[PARSING_STACK_SIZE];
-    size_t val_i = 0;
-    char scope_stack[PARSING_STACK_SIZE];
-    size_t scope_i = 0;
-
     // TODO
-
-    return 0;
 }
 
 
@@ -127,6 +168,7 @@ Cfg_Config *cfg_init(void)
     Cfg_Config *cfg = malloc(sizeof(Cfg_Config));
     cfg->global.vars = malloc(INITIAL_VARIABLES_NUM * sizeof(Cfg_Variable));
     cfg->global.vars_len = 0;
+    cfg->global.vars_cap = INITIAL_VARIABLES_NUM;
     cfg->err.type = CFG_ERROR_NONE;
     cfg->err.message = NULL;
     return cfg;
@@ -145,8 +187,8 @@ void cfg_destroy(Cfg_Config *cfg)
 
 int cfg_load_file(Cfg_Config *cfg, const char *file_path)
 {
-    if ((_cfg_open_file(cfg, file_path)) != 0 ||
-        (_cfg_parse_file(cfg) != 0)) {
+    if ((cfg__open_file(cfg, file_path)) != 0 ||
+        (cfg__parse_file(cfg, &cfg->global) != 0)) {
         return 1;
     }
     return 0;
--- example.c
+++ example.c
@@ -1,9 +1,14 @@
 #define CFG_IMPLEMENTATION
 #include "cfg.h"
 
+#include <stdio.h>
+
 int main(void)
 {
     Cfg_Config *cfg = cfg_init();
+
+    cfg_load_file(cfg, "./example.cfg");
+
     cfg_destroy(cfg);
     return 0;
 }
